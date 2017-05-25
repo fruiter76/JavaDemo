8b9e18877d4b87efcef7aa284be34813b33edc14
wubin <wubin@WUBIN-WIN10>
2017-05-25 23:31:37 +0800

    commit at home

--- src/cc/fruiter/demo/Demo.java
+++ src/cc/fruiter/demo/Demo.java
@@ -3,7 +3,7 @@ package cc.fruiter.demo;
 public class Demo {
 
 	public static void main(String[] args) {
-        System.out.println("Hello Git!!!");
+        System.out.println("Hello Git from home!!!");
 	}
 
 }
