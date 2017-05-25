52fe321df1a99da644f771b84e88cdcebc5ebe0a
fruiter76 <fruiter76@gmail.com>
2017-05-26 00:07:00 +0800

    third try at home

--- src/cc/fruiter/demo/Demo.java
+++ src/cc/fruiter/demo/Demo.java
@@ -3,7 +3,7 @@ package cc.fruiter.demo;
 public class Demo {
 
 	public static void main(String[] args) {
-        System.out.println("Hello Git from my Home!!!");
+        System.out.println("Hello Git From My Home!!!");
 	}
 
 }
